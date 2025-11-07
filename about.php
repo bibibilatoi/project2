4919d95b97900be1a7e07a602a090dd1b15897cb
Anh Tuan <trinhthaianhtuan@gmail.com>
2025-11-07 09:36:33 +0700

    done the prototype of manage.php . Adjusted the header.inc to allow devs to add their own stylesheets more easily. done the protype of login.php

---
+++ about.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="Advertise for vacant positions in a food website">
+      <meta name="keywords" content="HTML5, tags">
+      <meta name="author" content="a group of students">
+      <link href="styles/styles.css" rel="stylesheet">
+      <link href="styles/about_style.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo+2">
+      <title>About Us</title>
+  </head>
+  <body id ="about-body">
+    <header>
+      <nav id="nav-menu">
+          <input type="checkbox" id="nav-menu-toggle">
+          <label for="nav-menu-toggle">Menu</label>
+          <ul>
+            <li><a href="index.html">Home Page</a></li>
+            <li><a href="jobs.html">Jobs Positions</a></li>
+            <li><a href="apply.html">Applications</a></li>
+            <li><a id="current-page" href="#">About Us</a></li>
+            <li><a href="mailto:info@speedx.com.au"><strong>Contact us:</strong> info@speedx.com.au</a></li>
+          </ul>
+      </nav>
+
+      <div id="logo">
+        <img  src="images/speedx_logo.png" height="80" width="80" alt="speedX logo" >
+        <p>SpeedX</p>
+      </div>
+      
+      <section id="banner">
+          <p>
+            SpeedX - Bridging innovation and the infinite 
+          </p>
+      </section>
+    </header>
+    <main>
+
+      <h2 id="Our-tutor">Our tutor: Vu Ngoc Binh</h2> <!--Our tutor's name-->
+      <!--Students id here-->
+      <section id="about-student-section">
+        <p id="our-class">Class COS10026.2 - Friday 2pm</p> <!--Our tutor's name-->
+
+        <ul>
+          <li class = "about-student-id">
+            <ul>
+              <li>Trinh Thai Anh Tuan</li>
+              <li>ID: SWH03039</li>
+            </ul>
+          </li>
+          
+          <li class = "about-student-id">
+            <ul>
+              <li>Vu Hoang Long</li>
+              <li>ID: SWH03135</li>
+            </ul>
+          </li>
+          
+          <li class = "about-student-id">
+            <ul>
+              <li>Duong Tri Duc</li>
+              <li>ID: SWH02992</li>
+            </ul>
+          </li>        
+          
+          <li class = "about-student-id">
+            <ul>
+              <li>Dang Thien An</li>
+              <li>ID: SWH03458</li>
+            </ul>
+          </li>
+        </ul>
+
+      </section>
+      <!-- Our particular responsibilities -->
+      <h2 id="about-Group7-member-contribution">Members' Contributions</h2>
+      <section id="Members-contributions">
+        <!--Our Group photo-->
+        <figure id="about-figure-group-img">
+          <img id="group-image" src="images/group-image.jpg"  alt="Group 7 photo">
+          <figcaption>Our group photo</figcaption>
+        </figure>
+        <dl>
+          <dt>Trinh Thai Anh Tuan</dt>
+          <dd>Shared Responsibility: Home Page, CSS File</dd>
+          <dd>Individual Responsibility: About Us page (about.html)</dd>
+
+          <dt>Vu Hoang Long</dt>
+          <dd>Shared Responsibility: Home Page, CSS File</dd>
+          <dd>Individual Responsibility: Job Positions page (jobs.html)</dd>
+
+          <dt>Duong Tri Duc</dt>
+          <dd>Shared Responsibility: Home Page, CSS File</dd>
+          <dd>Individual Responsibility: Job Applications page, some job positions' descriptions</dd>
+          <dd>Question Poster</dd>
+
+          <dt>Dang Thien An</dt>
+          <dd>Shared Responsibility: Home Page, CSS File</dd>
+          <dd>Question Poster</dd>
+        </dl>
+
+
+      </section>
+
+
+      <!--Our interest table-->
+      <table class="about-table">
+        <caption>Our team's interests</caption>
+        <tr>
+          <th class="diagonal-cell"></th>
+          <th colspan="2">Books</th>
+          <th>Music</th>
+          <th>Films</th>
+          <th colspan="2">Games</th>
+        </tr>
+        <tr>
+          <td >Trinh Thai Anh Tuan</td>
+          <td >Harry Potter</td>
+          <td >Thinking, Fast and Slow</td>
+          <td >J-rock</td>
+          <td >The Lord of the Rings</td>
+          <td >ELden Ring</td>
+          <td >Roblox</td>
+        </tr>
+        <tr>
+          <td  >Vu Hoang Long</td>
+          <td >Harry Potter</td>
+          <td >LIFE 3.0</td>
+          <td >J-pop</td>
+          <td >Breaking bad</td>
+          <td >Expedition 33</td>
+          <td >Outer Wilds</td>
+        </tr>
+        <tr>
+          <td >Duong Tri Duc</td>
+          <td >The school of life</td>
+          <td >The Lord of The Rings</td>
+          <td >Pop</td>
+          <td >The revenant</td>
+          <td >Granny</td>
+          <td >Counter Strike</td>
+        </tr>
+        <tr>
+          <td >Dang Thien An</td>
+          <td >The 48 laws of power</td>
+          <td >Lolita</td>
+          <td >Jazz</td>
+          <td >Transformer</td>
+          <td >Wuthering Waves</td>
+          <td >StarCraft II</td>
+        </tr>
+      </table>   
+        
+    </main>
+    <footer>
+    <p><strong>2025 SpeedX</strong> - "Bridging innovation and the infinite."</p>
+    
+    <div>
+        <p><strong>SpeedX</strong></p>
+        <img src="images/speedx_logo.png" height="80" width="80" alt="speedX logo">
+    </div>
+    
+    <div id="contact-info">
+        <a href="mailto:info@speedx.com.au"><strong>Contact us:</strong> info@speedx.com.au</a>
+        <p><strong>Address:</strong> 42 Galaxy Road, Orbit City</p>
+        <p><strong>Phone:</strong> +1-800-555-SpeedX</p>
+        <a href="https://trinhthaianhtuan.atlassian.net/jira/software/projects/HCFPP1/boards/35/timeline?" target="_blank">
+            <strong>Visit our Jira project</strong>
+        </a>
+        
+        <a href="https://github.com/bibibilatoi/project1" target="_blank">
+            <strong>Visit the GitHub repo of this project</strong>
+        </a>
+        <p>&copy; 2025 SpeedX. All rights reserved.</p>
+    </div>
+</footer>
+  </body>
+</html>
