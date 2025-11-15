273301a00e1778228a178ba793bb8047679a73e5
dangthienan992-collab <dangthienan992@gmail.com>
2025-11-15 21:58:56 +0700

    Update database name to project2_db and redesign UI

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 $host = "localhost";        
 $user = "root";         
 $pwd = "";              
-$sql_db = "eoi";
+$sql_db = "project2_db";
 $conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
 if (!$conn) {
